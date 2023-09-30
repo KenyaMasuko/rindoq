884f220474af0a67a7179de8a78212aa4d4a93ba
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-09-30 18:59:38 +0900

    feat: migrate

---
+++ src/lib/db/migrations/0000_abandoned_dark_beast.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `posts` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`content` text NOT NULL,
+	`author_id` serial AUTO_INCREMENT NOT NULL,
+	CONSTRAINT `posts_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`email` text NOT NULL,
+	`name` text NOT NULL,
+	CONSTRAINT `users_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE INDEX `author_id` ON `posts` (`author_id`);
