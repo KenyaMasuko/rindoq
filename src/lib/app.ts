a7683468d89bb02cd783c9e306be4d5698d2c58a
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-09-30 19:02:02 +0900

    feat: add db connection

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,12 +1,12 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
+import * as schema from "./db/schema";
 
-// create the connection
 const connection = connect({
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
+  url: process.env.DATABASE_URL,
 });
 
-const db = drizzle(connection);
+export const db = drizzle(connection, { schema });
