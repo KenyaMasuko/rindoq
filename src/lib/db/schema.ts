a5af15e101f0d7eee560bb6058aadb8097a7c573
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-09-30 18:52:59 +0900

    feat: add db schema

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,37 @@
+import { InferInsertModel, InferSelectModel, relations } from "drizzle-orm";
+import { index, mysqlTable, serial, text } from "drizzle-orm/mysql-core";
+
+export const users = mysqlTable("users", {
+  id: serial("id").primaryKey(),
+  email: text("email").notNull(),
+  name: text("name").notNull(),
+});
+
+export const posts = mysqlTable(
+  "posts",
+  {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    content: text("content").notNull(),
+    authorId: serial("author_id").notNull(),
+  },
+  (table) => ({
+    authorIdIndex: index("author_id").on(table.authorId),
+  })
+);
+
+export const userRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
+
+export const postRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
+
+export type User = InferSelectModel<typeof users>;
+export type NewUser = InferInsertModel<typeof users>;
+export type Post = InferSelectModel<typeof posts>;
+export type NewPost = InferInsertModel<typeof posts>;
