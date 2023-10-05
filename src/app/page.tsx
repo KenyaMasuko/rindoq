c2c453e1d940c808c774204655e7e3322f779ca7
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-10-06 08:06:24 +0900

    refactor: クイズが見つからなかった時のUIを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
-import { QuizList } from "@/app/QuizList";
-import { SimpleGrid, Skeleton } from "@mantine/core";
+import { Anchor, SimpleGrid, Skeleton } from "@mantine/core";
 import { Metadata } from "next";
 import { Suspense } from "react";
+import { getQuizzes } from "@/lib/getQuizzes";
+import { Avatar, Button, Card, Group, Stack, Text } from "@mantine/core";
+import Link from "next/link";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "りんどQ | くいず一覧",
   description: "輪読会で読んだ内容をクイズにしてみんなで遊んじゃおう！",
 };
 
-const Page = () => {
+const Page = async () => {
+  const quiz = await getQuizzes({
+    includePrivateQuiz: false,
+  });
+
+  if (quiz.length === 0)
+    return (
+      <>
+        <Group justify="center" mt="md" mb="xs" w="100%">
+          <Stack align="center">
+            <Text fw="bold">
+              まだくいずがありません。イチバンにくいずを作成しよう！
+            </Text>
+            <Link href="/quiz/create" passHref>
+              <Anchor>くいずを作成する</Anchor>
+            </Link>
+          </Stack>
+        </Group>
+      </>
+    );
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} spacing="md">
       <Suspense
@@ -16,7 +38,38 @@ const Page = () => {
           <Skeleton key={i} height={250} width={300} />
         ))}
       >
-        <QuizList />
+        {quiz.map((x) => (
+          <Card
+            key={x.id}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            w="100%"
+            withBorder
+          >
+            <Stack justify="space-between" h="100%">
+              <Group justify="space-between" mt="md" mb="xs">
+                <Text fw={500}>{x.title}</Text>
+                <Avatar src={x.creatorImage} />
+              </Group>
+
+              <Text size="sm" c="dimmed">
+                {x.description}
+              </Text>
+              <Link href={`/quiz/${x.id}`}>
+                <Button
+                  variant="light"
+                  color="blue"
+                  fullWidth
+                  mt="md"
+                  radius="md"
+                >
+                  詳細
+                </Button>
+              </Link>
+            </Stack>
+          </Card>
+        ))}
       </Suspense>
     </SimpleGrid>
   );
